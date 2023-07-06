aa4a8950891bbe4a96d730d28f2d93b8b1ad8c0c
njagiiii <grace.makena@student.moringaschool.com>
2023-07-06 08:37:37 -0400

    complited assignment

--- index.js
+++ index.js
@@ -7,7 +7,85 @@
 
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
+ 
  */
+function createEmployeeRecord(arr){
+    let arroObj ={
+        firstName:arr[0],
+        familyName:arr[1],
+        title:arr[2],
+        payPerHour:arr[3],
+        timeInEvents:[],
+        timeOutEvents:[],
+    }
+    return arroObj;
+}
+function createEmployeeRecords(employeesArr){
+    let newArrObj = [];
+    employeesArr.forEach(employee => {
+        let newObj = createEmployeeRecord(employee);
+        newArrObj.push(newObj)
+    })
+    return newArrObj
+}
+function createTimeInEvent(date){
+    let datesplit = date.split('');
+    let addTimeIn ={
+        type:"TimeIn",
+        hour:parseInt(datesplit[1],10),
+        date:datesplit[0],
+    }
+    this.timeInEvents.push(addTimeIn)
+    return this;
+}
+function createTimeOutEvent(addTimeOut){
+    return this;
+}
+function hoursWorkedOnDate(selectDate){
+    let timeInEventsArr = this.timeInEvents
+    let timeOutEventsArr = this.timeOutEvents
+    let dayTimeIn;
+    let dayTimeOut;
+    timeInEventsArr.map(item =>{
+        if(item.date === selectDate){
+            dayTimeIn =item.hour
+        }
+    })
+    timeOutEventsArr.map(item => {
+        if(item.date === selectDate){
+            dayTimeOut =item.hour
+        }
+    })
+    let hoursWorked = Math.ceil((dayTimeOut-dayTimeIn)/100)
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(selectDate){
+    const hours = hoursWorkedOnDate.bind(this);
+    let hoursWork = hours(selectDate);
+    let wages = hoursWork*this.payPerHour;
+    return wages;
+}
+function findEmployeeByFirstName(srcArray,firstName){
+    let matchFind = srcArray.filter (item=>{
+        if(item.firstName === firstName){
+            return item
+        }
+    })
+    return matchFind
+}
+function calculatePayroll(arr){
+    let eachWage=[]
+    arr.map(obj => {
+        let x = allWagesFor.call(obj);
+        eachWage.push(x)
+    })
+    let totalWages =eachWage.reduce((accum, item) => {
+        return accum + item;
+    })
+    return totalWages
+}
+
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
